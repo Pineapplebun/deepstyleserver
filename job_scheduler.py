f6ae9e256a0419e064530b65e8a94304396d4710
pineapplebun <kevinjr.chen1@gmail.com>
2017-11-09 11:58:07 -0500

    changed is_empty to empty

--- job_scheduler.py
+++ job_scheduler.py
@@ -140,7 +140,7 @@ class job_scheduler(object):
         NOTE: NEED TO CHANGE THE TENSORFLOW EVALUATE SCRIPT TO ALLOW
         SOFT PLACEMENT IN THE SESSION.
         """
-        if not gpu_free.is_empty():
+        if not gpu_free.empty():
             job_to_run = self.job_queue.get()
             job_to_run.gpu = gpu_free.get()
 
@@ -184,8 +184,8 @@ class job_scheduler(object):
             self.create_jobs_and_queue()
 
             # When a job exists in the job queue
-            if not self.job_queue.is_empty():
-                while not gpu_free.is_empty():
+            if not self.job_queue.empty():
+                while not gpu_free.empty():
                     self.assign_gpu_and_run()
 
             # Check the processes that are running
