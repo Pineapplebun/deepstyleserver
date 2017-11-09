62b7b639a80a1a907523e3455a18d72e63cfc814
protoxpire0 <fahim.shahreer95@gmail.com>
2017-11-09 16:43:27 -0500

    fixed update

--- job_scheduler.py
+++ job_scheduler.py
@@ -92,6 +92,7 @@ class job_scheduler(object):
 
         try:
             self.db = sqlite3.connect(name_db)
+            self.db.row_factory = sqlite3.Row
         except Exception as e:
             print(e)
             sys.exit()
@@ -113,13 +114,14 @@ class job_scheduler(object):
         # checking
         #if len(c.fetchall()) == 0:
         #    print("cannot find any jobs")
-
-        for row in c.fetchall():
-            print("inside row in create")
+        row = c.fetchone()
+        while row is not None:
+            print(row.keys())
+            """
             self.job_queue.put(job(entry_id=c.lastrowid,
-                              path_to_im1=row['input_image'].image_path.url,
-                              path_to_im2=row['style_image'].image_path.url,
-                              output_path=row['output_image'].image_path.url,
+                              path_to_im1=row['input_image_id'].image_path.url,
+                              path_to_im2=row['style_image_id'].image_path.url,
+                              output_path=row['output_image_id'].image_path.url,
                               content_weight=row['content_weight'],
                               content_blend=row['content_weight_blend'],
                               style_weight=row['style_weight'],
@@ -129,12 +131,13 @@ class job_scheduler(object):
                               preserve_colors=row['preserve_colors'])
                           )
 
+            """
             # Set queue status of current row's id to be 'queued'
-            c.execute("UPDATE deepstyle_job SET job_status='P' WHERE rowid = %d" % c.lastrowid)
+            c.execute("UPDATE deepstyle_job SET job_status='P' WHERE rowid = %d" % row['id'])
             new_job_exists = True
-            self.logger.log.info("Job %d set In Progress" % c.lastrowid)
+            self.logger.log.info("Job %d set In Progress" % row['id'])
             print("ran create")
-
+            row = c.fetchone()
         c.close()
 
         if new_job_exists:
@@ -218,14 +221,14 @@ class job_scheduler(object):
                     gpu_free.put(completed_job.gpu)
 
                     # Change status of job in database
-                    c.execute("UPDATE deepstyle_job SET job_status='C' WHERE rowid = %s" % c.lastrowid)
+                    c.execute("UPDATE deepstyle_job SET job_status='C' WHERE rowid = %s" % row['id'])
 
                     self.logger.log.info(job_i)
                     break
 
             if exit_code != 0 and completed_job is not None:
                 print("Error in Popen")
-                c.execute("UPDATE deepstyle_job SET job_status='F' WHERE rowid = %s" % c.lastrowid)
+                c.execute("UPDATE deepstyle_job SET job_status='F' WHERE rowid = %s" % row['id'])
                 self.logger.log.error(job_i)
 
             # close cursor
