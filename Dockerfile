c9b4cdeafe37e4d386fa7ed23eb42ff095a7abfd
protoxpire0 <fahim.shahreer95@gmail.com>
2017-11-10 16:10:34 -0500

    made Dockerfile more efficient and fixed bugs in sheduler

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM tensorflow/tensorflow:latest-gpu-py3
 
-COPY dswebsite /app/dswebsite
-COPY neural-style/ /app/neural_style
-COPY start_app_wrapper.sh /app
-COPY job_scheduler.py /app
-COPY requirements.txt /app
+RUN mkdir /app
+COPY requirements.txt /app/
 
 WORKDIR /app
 
 RUN pip3 install -r requirements.txt
 
 EXPOSE 8000
+
+COPY dswebsite /app/dswebsite
+COPY neural-style/ /app/neural_style
+COPY start_app_wrapper.sh /app
+COPY job_scheduler.py /app
+
--- job_scheduler.py
+++ job_scheduler.py
@@ -35,9 +35,9 @@ class job(object):
                  preserve_color):
 
         self.job_id = j_id
-        self.path1 = INPUT_FILE_PATH + im_name1
-        self.path2 = INPUT_FILE_PATH + im_name2
-        self.output_path = OUTPUT_FILE_PATH + output_name
+        self.path1 = "%s/%s" % (INPUT_FILE_PATH, im_name1)
+        self.path2 = "%s/%s" % (INPUT_FILE_PATH, im_name2)
+        self.output_path = "%s/job_%d.jpg" % (OUTPUT_FILE_PATH, j_id)
         self.content_weight = content_weight
         self.content_blend = content_blend
         self.style_weight = style_weight
@@ -54,7 +54,7 @@ class job(object):
         self.finished = None
 
     def __str__(self):
-        return "%s : %s : %s" % (self.path1, self.path2, self.out)
+        return "%s : %s : %s" % (self.path1, self.path2, self.output_path)
 
 class logger(object):
     """
@@ -179,9 +179,9 @@ class job_scheduler(object):
         NOTE: NEED TO CHANGE THE TENSORFLOW EVALUATE SCRIPT TO ALLOW
         SOFT PLACEMENT IN THE SESSION.
         """
-        if not gpu_free.empty():
+        if not self.gpu_free.empty():
             job_to_run = self.job_queue.get()
-            job_to_run.gpu = gpu_free.get()
+            job_to_run.gpu = self.gpu_free.get()
 
             # Create a copy of the environemnt
             new_env = os.environ.copy()
@@ -196,26 +196,28 @@ class job_scheduler(object):
             # Run the subprocess
             try:
                 job_to_run.proc = Popen(['python',
-                                         'neural_style.py',
+                                         '/app/neural-style/neural_style.py',
                                          '--content', '%s' % job_to_run.path1,
                                          '--styles', '%s' % job_to_run.path2,
                                          '--output','%s' % job_to_run.output_path,
-                                         '--content-weight', job_to_run.content_weight,
-                                         '--content-weight-blend', job_to_run.content_blend,
-                                         '--style-weight', job_to_run.style_weight,
-                                         '--style-layer-weight-exp', job_to_run.style_layer_weight_exp,
-                                         '--style-scales', job_to_run.style_scale,
-                                         '--iterations', job_to_run.iterations,
+                                         '--content-weight', str(job_to_run.content_weight),
+                                         '--content-weight-blend', str(job_to_run.content_blend),
+                                         '--style-weight', str(job_to_run.style_weight),
+                                         '--style-layer-weight-exp', str(job_to_run.style_layer_weight_exp),
+                                         '--style-scales', str(job_to_run.style_scale),
+                                         '--iterations', str(job_to_run.iterations),
                                          '%s' % preserve
                                          ], env=new_env)
                 self.logger.log.info("Job %d assigned GPU %d." % (job_to_run.job_id, job_to_run.gpu))
-                running_procs.append(job_to_run)
+                self.running_jobs.append(job_to_run)
             
             except Exception as e:
                 self.logger.log.error("Job %d could not be assigned GPU %d." % (job_to_run.job_id, job_to_run.gpu))
                 self.logger.log.exception(e)
                 c = self.db.cursor()
                 c.execute("UPDATE deepstyle_job SET job_status='PF' WHERE rowid = %d" % job_to_run.job_id)
+                self.gpu_free.put(job_to_run.gpu)
+
 
     def main(self):
         """
@@ -228,38 +230,43 @@ class job_scheduler(object):
             self.create_jobs_and_queue()
 
             # When a job exists in the job queue
-            if not self.job_queue.empty():
-                print("job queue is not empty")
-                while not gpu_free.empty():
+            if not self.job_queue.empty(): 
+                while not self.gpu_free.empty():
                     self.assign_gpu_and_run()
 
-            # Check the processes that are running
-            # If proccesses are still running then this loop will continue to
-            # run until a process is finished. Once a process has finished,
-            # we can check if any requests are made and then create jobs from
-            # them and we can check if any jobs are waiting. GPU bound.
             completed_job = None
             c = self.db.cursor()
+
+            # Need to set exit code to 0 since if no job then we won't exec
+            # the error handling and if there is a job it won't matter.
             exit_code = 0
+
+            # Loop will run until a job is finished
             for job_i in self.running_jobs:
-                # the proc could be None if the process doesn't exist
+
+                # job.proc could be None if the process doesn't exist
                 # but this is mitigated since no job in running_jobs will
                 # ever have None in its proc attribute
                 exit_code = job_i.proc.poll()
                 if exit_code is not None:
-                    completed_job = running_procs.remove(job_i)
-                    gpu_free.put(completed_job.gpu)
+                    completed_job = job_i
+                    # It is okay to remove this job from running_jobs because it will
+                    # break in this if statement anyways
+                    self.running_jobs.remove(job_i)
+                    self.gpu_free.put(completed_job.gpu)
 
                     # Change status of job in database
-                    c.execute("UPDATE deepstyle_job SET job_status='C' WHERE rowid = %s" % row['id'])
+                    c.execute("UPDATE deepstyle_job SET job_status='C' WHERE rowid = %s" % completed_job.job_id)
 
                     self.logger.log.info(job_i)
                     break
 
+            # Completed_job only None if exit_code is None which means it's still running
+            # If exit_code is not 0, then an error has occurred.
             if exit_code != 0 and completed_job is not None:
                 
                 # Remove job from executing by setting status to F
-                c.execute("UPDATE deepstyle_job SET job_status='F' WHERE rowid = %s" % row['id'])
+                c.execute("UPDATE deepstyle_job SET job_status='F' WHERE rowid = %s" % completed_job.job_id)
                 self.logger.log.error(str(job_i) + " failed to complete.")
 
             # close cursor
