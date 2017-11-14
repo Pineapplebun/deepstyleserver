7662dbcec14658f561449da08e0956b4f6519297
Ubuntu <ubuntu@ip-172-31-42-183.us-east-2.compute.internal>
2017-11-14 16:29:39 +0000

    fixed media url

--- start_app_wrapper.sh
+++ start_app_wrapper.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 python /app/dswebsite/manage.py makemigrations &&
+sleep 10 &&
 python /app/dswebsite/manage.py migrate &&
 cd dswebsite &&
 gunicorn dswebsite.wsgi -b 0.0.0.0:8000 &
-sleep 20 &&
-python3 /app/job_scheduler.py 1 &
+sleep 30 &&
+python /app/job_scheduler.py 16 &
 wait
