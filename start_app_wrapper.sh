19821b3437020399271779f4f1827e8ca035b760
Ubuntu <ubuntu@ip-172-31-18-22.us-east-2.compute.internal>
2017-11-16 18:22:37 +0000

    reversed order for list

--- start_app_wrapper.sh
+++ start_app_wrapper.sh
@@ -6,5 +6,5 @@ python /app/dswebsite/manage.py collectstatic --no-input &&
 cd dswebsite &&
 gunicorn dswebsite.wsgi -b 0.0.0.0:8000 &
 sleep 30 &&
-python /app/job_scheduler.py 1 &
+python /app/job_scheduler.py 8 &
 wait
