e8836cf3df71cd27c8b0298f5e1516279a27371a
Ubuntu <ubuntu@ip-172-31-38-53.us-east-2.compute.internal>
2017-11-16 08:13:55 +0000

    added collectstatic to script

--- start_app_wrapper.sh
+++ start_app_wrapper.sh
@@ -2,6 +2,7 @@
 sleep 10 &&
 python /app/dswebsite/manage.py makemigrations &&
 python /app/dswebsite/manage.py migrate &&
+python /app/dswebsite/manage.py collectstatic &&
 cd dswebsite &&
 gunicorn dswebsite.wsgi -b 0.0.0.0:8000 &
 sleep 30 &&
