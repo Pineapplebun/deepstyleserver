3edb186f940f2ebfd1720a936d9750b3ec3db54c
Ubuntu <ubuntu@ip-172-31-18-22.us-east-2.compute.internal>
2017-11-16 08:20:21 +0000

    no input for collectstatcic

--- start_app_wrapper.sh
+++ start_app_wrapper.sh
@@ -2,7 +2,7 @@
 sleep 10 &&
 python /app/dswebsite/manage.py makemigrations &&
 python /app/dswebsite/manage.py migrate &&
-python /app/dswebsite/manage.py collectstatic &&
+python /app/dswebsite/manage.py collectstatic --no-input &&
 cd dswebsite &&
 gunicorn dswebsite.wsgi -b 0.0.0.0:8000 &
 sleep 30 &&
